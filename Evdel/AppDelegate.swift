5ba395df5b881da922ed63db2ecd212fc6c96399
Sash Zats <sash@zats.io>
2015-06-20 12:12:56 -0700

    Initial project structure

---
+++ Evdel/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Evdel
+//
+//  Created by Sash Zats on 6/20/15.
+//  Copyright © 2015 Sash Zats. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
