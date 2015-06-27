cc58d28ae18df35e160ab6fe0111f87c1d1b14e5
Sash Zats <sash@zats.io>
2015-06-26 18:49:55 -0700

    window managment
    closing the app window closes the app
    opening the app brings it to the front

--- Evdel/AppDelegate.swift
+++ Evdel/AppDelegate.swift
@@ -11,15 +11,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-        
+        NSApp.activateIgnoringOtherApps(true)
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
     }
 }
 
